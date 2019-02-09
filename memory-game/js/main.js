33aefa96c7f5c3efe0dc6f03160d1928820b36f1
Richard Yarwood <richyarwood@gmail.com>
2019-02-09 14:48:42 +0000

    Added JS functions

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,15 +1,21 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-cardOne = cards[0];
-cardTwo = cards[2];
+var checkForMatch = function() {
 
-cardsInPlay.push(cardOne, cardTwo);
+	if (cardsInPlay.length === 2) {
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] ===  cardsInPlay[1]){
-		alert("You found a match");
-	} else {
-		alert("Try again");
-	}
-}
+		if (cardsInPlay[0] ===  cardsInPlay[1]) {
+			alert("You found a match");
+		} else {
+			alert("Sorry, try again");
+		};
+	};
+};
+
+var flipCard = function(cardId) {
+	
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+	checkForMatch();
+};
