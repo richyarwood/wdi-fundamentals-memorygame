0ee381ff321a334a33d62971c766246882472d6c
richy <richyarwood@gmail.com>
2019-02-11 21:31:04 +0000

    Added in click events

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -24,6 +24,30 @@ var cards = [
 
 var cardsInPlay = [];
 
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+var flipCard = function() {
+	var cardId = this.getAttribute('id');
+	this.setAttribute('src', cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	console.log(cardId);
+	checkForMatch();
+};
+//Bonus tasks
+//Add in a reset button
+//Keep track of the users score
+
 var checkForMatch = function() {
 
 	if (cardsInPlay.length === 2) {
@@ -36,11 +60,4 @@ var checkForMatch = function() {
 	};
 };
 
-var flipCard = function(cardId) {
-	
-	cardsInPlay.push(cards[cardId].rank);
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].suit);
-	console.log(cards[cardId].cardImage);
-	checkForMatch();
-};
+createBoard();
